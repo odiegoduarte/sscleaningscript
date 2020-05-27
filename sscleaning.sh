47510134acc5ef334e21663391d9872471dc1a11
odiegoduarte <dduartesouza17@gmail.com>
2020-05-27 15:31:15 -0300

    sscleaning v2

--- sscleaning.sh
+++ sscleaning.sh
@@ -2,9 +2,9 @@
 
 #Criado por Diego Duarte 2020
 
-# SSCleaning é um simples shell script que foi criado para fazer a limpeza 
+# SSCleaning V2 é um simples shell script que foi criado para fazer a limpeza 
 # automatizada do sistema, utilizando comandos do APT.
-# Esse shell script utiliza o zenity para gerar caixas de diálogo GTK.
+# Esse shell script utiliza o whiptail para gerar caixas de diálogo.
 
 echo -e "
 
@@ -16,50 +16,37 @@ echo -e "
 ╚══════╝╚══════╝ ╚═════╝╚══════╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝╚═╝  ╚═══╝ ╚═════╝                                                                                                                                                                                                                                                                                   
 
 "
-(
-echo "10" ; sleep 1
-echo "# Verificando cache do sistema" ; sleep 1
-echo "20" ; sleep 1
-echo "# Verificando runtimes obsoletas do Flatpak " ; sleep 1
-echo "50" ; sleep 1
-echo "Verificando pacotes .deb obsoletos" ; sleep 1
-echo "75" ; sleep 1
-echo "# Pronto para iniciar a limpeza do sistema" ; sleep 1
-echo "100" ; sleep 1
-) |
-zenity --progress \
-  --title="Iniciando" \
-  --text="Análise..." \
-  --percentage=0
 
-if [ "$?" = -1 ] ; then
-        zenity --error \
-          --text="Update."
+{
+    for ((i = 0 ; i <= 100 ; i+=7)); do
+        sleep 0.1
+        echo $i
+    done
+} | whiptail --gauge "Iniciando o SSCleaning" 6 50 0
+
+if (whiptail --title " Seja bem-vindo(a)$(whoami) " --yesno "SSCleaning é um simples shell script que foi criado para fazer a limpeza automatizada do sistema.
+No próximo passo irá te pedir a senha de usuário.
+Deseja continuar ?" 10 60) then
+    echo "Iniciando limpeza . . . "
+else
+    echo "Limpeza cancelada.$6 "
 fi
-
-echo "Seja bem-vindo(a)," $(whoami), "a limpeza automatizada do seu sistema já vai começar."
-
-
-zenity --question --text "Olá, no próximo passo irá te pedir a senha de usuario, Deseja continuar ?"
-
 
 ## "Definir o numero máximo de revisões de um snap armazenadas pelo sistema após a proxima atualização."
 sudo snap set system refresh.retain=2
 
 echo "Removendo runtimes obsoletas do flatpak" 
-flatpak uninstall --unused
+flatpak uninstall --unused -y
 
 echo "Removendo dependências obsoletas sistema."
-sudo apt-get autoremove
+sudo apt autoremove -y
 
 echo "Verificando cache do apt"
 sudo du -sh /var/cache/apt
 
-echo "Removendo pacotes .deb obsoletos."
-sudo apt-get autoclean
-
 echo "Removendo pacotes obsoletos."
-sudo apt-get clean
+sudo apt autoclean -y
+sudo apt clean -y
 
 echo "Limpando o cache de miniaturas."
 du -sh ~/.cache/thumbnails
@@ -68,6 +55,15 @@ echo "Limpando o cache do Spotify."
 sudo rm -rf ~/.cache/spotify/Data/*
 sudo rm -rf ~/.cache/spotify/Storage/*
 
-## Limpeza finalizada
-zenity --info --text "Limpeza finalizada !"
+##Instalando deborphan
+echo "Instalando deborphan e removendo pacotes órfãos."
+sudo apt install deborphan
+deborphan -s
+sudo apt remove $(deborphan)
+deborphan -s
+sudo apt remove $(deborphan)
+deborphan -s
+sudo apt remove $(deborphan)
 
+## Limpeza finalizada !
+echo "Limpeza finalizada !"
